c38385664d3293237a83439fd68b09b7b616e43d
LeMufas2a <codage.mufasa@gmail.com>
2021-02-19 02:37:51 +0100

    while

---
+++ exo_boucle_4/assets/js/main.js
@@ -0,0 +1,49 @@
+/* let compteur = 0;
+let chiffre = parseInt(prompt("Donnez un chiffre"))
+
+while (compteur <= chiffre) {
+    console.log(compteur);
+    compteur++
+} */
+
+/* let i = 0;
+let myArray = [
+    "abdel",
+    "amine",
+    "antoine",
+    "ayoub",
+    "bilal",
+    "camille",
+    "cem",
+    "gauthier",
+    "laurie",
+    "mahad",
+    "mufassa",
+    "nghia",
+    "redwane",
+    "salim",
+    "said",
+    "yahya",
+    "yasmina",
+    "yassin"
+]
+
+while (i < myArray.length) {
+    console.log("Bonjour " + myArray[i]);
+    i++
+}
+ */
+
+/* let myArray = [];
+
+let student = parseInt(prompt("Combien d'étudiant"))
+
+let i = 1;
+
+while (myArray.length < student) {
+    myArray.push("Student " + i)
+    i++
+}
+console.table(myArray); */
+
+
---
+++ exo_boucle_4/assets/js/while.js
@@ -0,0 +1,97 @@
+//WHILE
+// exo 1
+// let chiffre = parseInt(prompt("choisissez un chiffre :"));
+// let compteur = 0;
+
+// exo 2
+// let classe = [
+//   "Mohammed",
+//   "Yasmina",
+//   "Amine",
+//   "Salim",
+//   "Abdel",
+//   "Majda",
+//   "Bilal",
+//   "Laurie",
+//   "Yahya",
+//   "Mahad",
+//   "Nghia",
+//   "Saïd",
+//   "Camille",
+//   "Antoine",
+//   "Ayoub",
+//   "Yassin",
+//   "Cem",
+//   "Gauthier",
+//   "Redwane",
+// ];
+// let a = 0;
+
+// while (a < classe.length) {
+//   console.log("Bonjour " + classe[a]);
+//   a += 1;
+// }
+
+// exo 3
+// let classe = [];
+// let max = parseInt(prompt("Entre un nombre maximum :"));
+
+// while (classe.length < max) {
+//   classe.push(prompt("Entrez le nom d'un étudiant :"));
+// }
+
+// console.log("La classe :", classe);
+
+// DOWHILE
+// while (compteur <= chiffre) {
+//   console.log(compteur);
+//   compteur++;
+// }
+
+// // Exo 1
+// let chiffre1 = parseInt(prompt("Ajoutez un chiffre :"));
+// let compteur1 = 0;
+
+// do {
+//   compteur1++;
+//   console.log(compteur1);
+// } while (compteur1 <= chiffre1);
+
+// Exo2
+// let classe = [
+//   "Mohammed",
+//   "Yasmina",
+//   "Amine",
+//   "Salim",
+//   "Abdel",
+//   "Majda",
+//   "Bilal",
+//   "Laurie",
+//   "Yahya",
+//   "Mahad",
+//   "Nghia",
+//   "Saïd",
+//   "Camille",
+//   "Antoine",
+//   "Ayoub",
+//   "Yassin",
+//   "Cem",
+//   "Gauthier",
+//   "Redwane",
+// ];
+// let i = 0;
+
+// do {
+//   console.log("Bonjour " + classe[i]);
+//   i++;
+// } while (classe[i]);
+
+// Exo 3
+// let fruits = ["pomme", "banane", "fraise", "framboise", "orange", "cerise"];
+
+// do {
+//   fruits.shift();
+//   console.log(fruits);
+// } while (fruits.length);
+
+// console.log(fruits);
