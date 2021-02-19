3f168926edbf18e3523fce4b96d09d3e88e41263
LeMufas2a <codage.mufasa@gmail.com>
2021-02-19 02:42:01 +0100

    Switch

---
+++ exo_switch/assets/js/main.js
@@ -0,0 +1,99 @@
+// # Les switchs : exercices
+
+// ## 1. ToDo
+
+/* let jour = prompt("Dis moi un jour de la semaine").trim().toLocaleLowerCase()
+
+switch (jour) {
+    case "lundi":
+        alert("Lundi je vais coder")
+        break;
+    case "mardi":
+        alert("Mardi je vais faire du sport")
+        break;
+    case "mercredi":
+        alert("Mercredi je vais manger au restaurant")
+        break;
+    case "jeudi":
+        alert("Jeudi je suis en réunions")
+        break;
+    case "vendredi":
+        alert("Vendredi jour de paye")
+        break;
+    case "samedi":
+        alert("Samedi je fais la fête ")
+        break;
+    case "dimanche":
+        alert("Dimanche je me réveille la tête dans le cul")
+        break;
+
+
+    default:
+        alert("Tu n'as pas choisi de jour, dommage")
+        break;
+} */
+
+
+// ## 2. Sortez couverts
+
+/* let meteo = prompt("Que prévois la météo ? ")
+
+switch (meteo) {
+    case "pluie":
+        alert("Sort avec un parapluie")
+        break;
+    case "soleil":
+        alert("Sort avec ta crème solaire")
+        break;
+    case "froid":
+        alert("Sort avec une grosse veste")
+        break;
+    case "chaud":
+        alert("Sort avec tes lunettes de soleil")
+        break;
+
+    default:
+        alert("tu sais pas c'est quoi la météo ? ")
+        break;
+} */
+
+// ## 3. Les 2 font la pair
+
+/* let nombre = parseInt(prompt("donne un nombre"))
+
+switch (nombre % 2) {
+    case 0:
+        alert("Pair")
+        break;
+    default:
+        alert("Impair")
+        break;
+} */
+
+
+// ## 4. Maths "avancée"
+/* let x = parseInt(prompt("un premier nombre"))
+let y = prompt("un opérateur mathématique")
+let z = parseInt(prompt("un second nombre"))
+
+switch (y) {
+    case "/":
+        alert(+x + "/" + z + " = " + x / z)
+        break;
+
+    case "+":
+        alert(+x + "+" + z + " = " + (x + z))
+        break;
+
+    case "-":
+        alert(+x + "-" + z + " = " + (x - z))
+        break;
+
+    case "*":
+        alert(+x + "*" + z + " = " + x * z)
+        break;
+
+    default:
+        break;
+}
+ */
