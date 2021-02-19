14dee72df5b42701cf5c26344842b0fb71df22e9
LeMufas2a <codage.mufasa@gmail.com>
2021-02-19 02:35:52 +0100

    String => Number

---
+++ convert_str_to_int/public/js/main.js
@@ -0,0 +1,11 @@
+// let age = parseInt(prompt("Quel est votre âge ?"))
+// let age = Number(prompt("Quel est votre âge ?"))
+let âge = prompt("Ton âge ?")
+console.log(typeof âge);
+âge = Number(âge)
+console.log(typeof âge);
+
+let year = prompt("Donne une année")
+let annee = prompt("Annee de naissance")
+x = year-annee
+alert("en " + year + " tu auras " + x + " ans ")
