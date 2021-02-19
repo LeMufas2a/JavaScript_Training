f863ae6ce489fb86253b836edc492f78b9ba2832
LeMufas2a <codage.mufasa@gmail.com>
2021-02-19 02:37:59 +0100

    do while

---
+++ exo_boucle_5/assets/js/main.js
@@ -0,0 +1,75 @@
+// Exo 1
+
+/* let number = prompt("Donne un nombre")
+let compteur = 0;
+
+do {
+    console.log(compteur);
+    compteur++
+
+} while (compteur <= chiffre)
+ */
+
+// Exo 2 
+/* let myArray = [
+    "abdel",
+    "amine",
+    "antoine",
+    "ayoub",
+    "bilal",
+    "camille",
+    "cem",
+    "gauthier",
+    "laurie",
+    "mahad",
+    "mufassa",
+    "nghia",
+    "redwane",
+    "salim",
+    "said",
+    "yahya",
+    "yasmina",
+    "yassin",
+]
+
+let i = 0;
+
+
+do {
+    console.log("Bonjour " + myArray[i]);
+    i++
+} while (i < myArray.length) */
+
+// Exo 3
+
+/* let boobs = [
+    "Kiwi",
+    "Wiski",
+    "Dattes",
+    "Zatte",
+    "Banane",
+]
+
+
+do {
+    let pic = boobs.splice(0, 1);
+    console.log(boobs);
+} while (boobs.length > 0) */
+
+
+let x = [
+    "Banane",
+    "Pomme",
+    "Cerise",
+    "Peche",
+    "Poire",
+    "Orange",
+]
+
+let i = 0
+
+do {
+    console.log(x);
+    i--;
+
+} while (x.shift());
