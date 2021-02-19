327919e95790b09482fa3e5248e792b254a0c0b5
LeMufas2a <codage.mufasa@gmail.com>
2021-02-19 02:45:19 +0100

    Variable

---
+++ javascript_les_variables_exo_02/main.js
@@ -0,0 +1,24 @@
+/* let phrase = "J'aime les tartines et j'en mange"
+
+console.log(phrase);
+
+let jour = "4 par jours"
+console.log(phrase + ' ' + jour);
+
+let cinq = 5
+let chiffre = 35
+console.log(cinq + chiffre);
+console.log(phrase + " " + cinq); */
+
+/* let salut = 'Salut tout le monde'
+console.log(salut);
+
+let ciao = 'Au revoir !'
+console.log(ciao);
+
+salut = ciao
+console.log(salut); */
+
+// let letter = '6'
+// let chiffre = 4
+// console.log(chiffre  letter);
